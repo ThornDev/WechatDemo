4f8ab7823fa0070519a8638e9f4e667c617f6589
ThornDev <jiemianwang@gmail.com>
2017-03-14 18:50:12 +0800

    add files

--- index.php
+++ index.php
@@ -1,23 +1,29 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN">
 <html>
 <head>
+<title>微信公众号管理系统</title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<title>微信公众号管理</title>
+<meta name="Designer" content="PremiumPixels.com">
+<meta name="Author" content="$hekh@r d-Ziner, CSSJUNTION.com">
+<link rel="stylesheet" type="text/css" href="css/reset.css">
+<link rel="stylesheet" type="text/css" href="css/structure.css">
 </head>
- <body>
- <img src="system_bg.jpg" width="100%" height="30%"/>  
- <form action="views/setWeMenu.php" method="post">
-   <p>微信公众号管理系统!</p>
-   <br>
-  username: <input type="text" name="appid" value="" >
-  <br>
-  password:<input type="password" name="appsecret">
-   <br>
-   <input type="submit" value="登录">
-    <?php
-  
-	?>
+
+<body>
+<form class="box login" action="views/setWeMenu.php" method="post">
+	<fieldset class="boxBody">
+	  <label>用户名</label>
+	  <input type="text" name="username" tabindex="1" placeholder="用户名" required>
+	  <label><a href="#" class="rLink" tabindex="5">忘记密码？</a>密码</label>
+	  <input type="password" name="password" tabindex="2" placeholder="密码">
+	</fieldset>
+	<footer>
+	  <label><input type="checkbox" tabindex="3">记住密码</label>
+	  <input type="submit" class="btnLogin" value="登 录" tabindex="4">
+	</footer>
 </form>
-   </body>
-   
+<footer id="main">
+  <a href="http://thornvbear.tech/about/">thornvbear.tech</a> | <a href="http://thornvbear.tech/about/">PSD by thornvbear</a>
+</footer>
+</body>
 </html>
